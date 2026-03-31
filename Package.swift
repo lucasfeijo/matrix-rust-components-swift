f59963510654adeffb8d8f68776f10fb6d8ec5ee
Lucas Feijo <lucas.feijo@gmail.com>
2026-03-31 19:03:54 -0300

    Fix module discovery for Xcode 26.4 / Swift 6.3
    Repackage xcframework to flatten Headers/MatrixSDKFFI/ into Headers/
    so that SPM's -I flag finds the module.modulemap directly.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Fork of matrix-org/matrix-rust-components-swift with repackaged xcframework
+// to fix module discovery with Xcode 26.4 / Swift 6.3.
+// The original xcframework nests the module.modulemap inside Headers/MatrixSDKFFI/,
+// which SPM's -I flag alone can no longer resolve. This fork flattens the headers
+// so that -I Headers/ finds the modulemap directly.
 import PackageDescription
-let checksum = "49e0324449233c6982db2a0f6da06f9aa18567546b49eee8169c1badb6493cc3"
-let version = "26.01.04"
-let url = "https://github.com/matrix-org/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
+let checksum = "92370f71e4328b657474ccc6c1e39d0d01f9de1376a3e21bd92732ff7ee5d0ea"
+let version = "26.01.04-fixed"
+let url = "https://github.com/lucasfeijo/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
     platforms: [
